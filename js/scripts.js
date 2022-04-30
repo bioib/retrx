16aa7ccbc0ec65ff6ba887821b0fd8f4a997a4c1
Bio <revandagaming@gmai.com>
2022-05-01 04:58:00 +0700

    add smooth scroll effects for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -56,4 +56,45 @@ window.addEventListener('DOMContentLoaded', event => {
         elements: '#portfolio a.portfolio-box'
     });
 
+    // Scroll
+    function scrollTo(element) {
+      document.querySelector(element).scrollIntoView({behavior: "smooth"})
+    }
+    
+    const ctaBtn = document.getElementById("ctaBtn");
+
+    $('#headerBtn').on('click', function(e) {
+      $('html, body').animate({
+        scrollTop: $(window).height()
+      }, 600)
+    })
+
+    $('#ctaBtn').on('click', function(e) {
+      $('html, body').animate({
+        scrollTop: $(window).height() + $(this).parents('section').outerHeight()
+      }, 600)
+      // console.log($(this).parents('section').outerHeight())
+    })
+
+    $('.nav-link').on('click', function(e) {
+      // console.log($(this).attr('href'))
+      $('html, body').animate({
+        scrollTop: $(window).height() + $($(this).attr('href')).outerHeight()
+      })
+    })
+
+    const a = document.getElementsByClassName("nav-link");
+
+    // console.log(Array(a[4].getAttribute('href')))
+
+    for(const b of a) {
+      b.addEventListener('click', function(e) {
+        e.preventDefault();
+        scrollTo(b.getAttribute('href'))
+      })
+    }
+
+    
+
+
 });
